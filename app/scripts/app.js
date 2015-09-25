54147a598967617eebd6a787cc24f8c062b19a19
EricSSartorius <EricSSartorius@gmail.com>
2015-09-25 23:04:03 +0900

    timer and button working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,38 +18,30 @@ angular.module('blocPomodoro', ['firebase', 'ui.router'])
 .controller('Home.controller', ['$scope', '$firebaseObject', '$interval', function ($scope, $firebaseObject, $interval) {
 	var ref = new Firebase("https://blinding-torch-8353.firebaseio.com");
     $scope.data = $firebaseObject(ref);
-    $scope.theTime = 1500;
-
- //    $scope.startTime = function() {
- //         stop = $interval(function() {
- //            if ($scope.theTime > 0 ) {
- //              $scope.theTime -= $scope.theTime;
- //            } else {
- //              $scope.resetTime();
- //            }
- //          }, 1000);
 }])
 .directive('myButton', function($interval) {
 
     return {
-        templateUrl: 'templates/start.html',
+        template: '<div><h1>{{ theTime | timeFilter | date:"mm:ss"}}</h1><button ng-click="clickStart()">{{theButton}}</button><div>',
         restrict: 'E',
         replace: true,
         scope: { },
         link: function(scope, element, attributes) {
-        	scope.theButton = "START";
-            scope.clickStart = function (event) {
-            	if(scope.theButton == "RESET") {
-            		//Reset theTime to 1500 here
-            		//Stop the interval counting down here
-            		scope.theButton = "START";
-            	}
-            	else {
-            		//Start the interval counting down here
-            		scope.theButton = "RESET";
-            	}
-            };
-        }    
+        	scope.theTime = 1500;
+    		scope.theButton = "START";
+    		var theTimer;
+		    scope.clickStart = function() {
+		        if(scope.theButton == "RESET") {
+		           	scope.theTime = 1500;
+		           	$interval.cancel(theTimer);
+		           	scope.theButton = "START";
+		        }
+		        else {
+		           	theTimer = $interval(function(){scope.theTime--},1000,0);
+		           	scope.theButton = "RESET"; 
+		        }   
+			};
+    	}
     }
  })
 .filter('timeFilter', function() {
